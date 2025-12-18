d50127abe7fa5f2a4e1d749b99d4f6790f92f75b
tbreslein <tommy.breslein@protonmail.com>
2025-12-18 15:07:58 +0100

    small changes

--- keyboards/bastardkb/charybdis/4x6/keymaps/mykeymap/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/mykeymap/keymap.c
@@ -73,16 +73,16 @@ static uint16_t auto_pointer_layer_timer = 0;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT(
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-       KC_GRV,  KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR,  KC_TAB, LSFT(KC_TAB), KC_TILD,
+       QK_GESC, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, DRGSCRL,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_ESC,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS,
+        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        KC_MINS,    HM_A,    HM_S,    HM_D,    HM_F,    KC_G,       KC_H,    HM_J,    HM_K,    HM_L, HM_SCLN, KC_QUOT,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        KC_LBRC,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RBRC,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                   KC_SPC, KC_BSPC, KC_LPRN,      KC_EQL, LT(FUNCS, KC_ENT),
-                                             KP_PT, KC_RPRN,      KC_DEL
+                                   KC_SPC, KC_BSPC,   KP_PT,     KC_EQL, LT(FUNCS, KC_ENT),
+                                           KC_BTN1, KC_BTN2,     KC_DEL
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
@@ -126,8 +126,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_BTN1,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                   KC_SPC, KC_BSPC, KC_LBRC,     KC_EQL, LT(FUNCS, KC_ENT),
-                                           KC_LALT, KC_RBRC,     KC_DEL
+                                   KC_SPC, KC_BSPC, KC_LALT,     KC_EQL, LT(FUNCS, KC_ENT),
+                                           KC_LBRC, KC_RBRC,     KC_DEL
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
